fef380ff65fa86a9dfdd0ce6205e773490a97285
evadevkota <devkotaeva@gmail.com>
2024-12-02 16:37:21 +0000

    data concating

--- st20307767/st20307767.py
+++ st20307767/st20307767.py
@@ -1,3 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+# Step 1: Load and concatenate data from multiple CSV files
+air_quality_data_set_file_paths = [
+    r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Aotizhongxin_20130301-20170228.csv',
+    r"C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Changping_20130301-20170228.csv",
+    r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Dingling_20130301-20170228.csv',
+  
+    r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Dongsi_20130301-20170228.csv',
+  ]
+
+data_set_frames = [pd.read_csv(file) for file in air_quality_data_set_file_paths]
+df = pd.concat(data_set_frames)
+
