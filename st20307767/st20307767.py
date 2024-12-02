c97d3c65bae8aeb3ab73e02e87fb82a74250c3d5
evadevkota <devkotaeva@gmail.com>
2024-12-02 17:00:50 +0000

    Statics value

--- st20307767/st20307767.py
+++ st20307767/st20307767.py
@@ -2,8 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-# Step 1: Load and concatenate data from multiple CSV files
+#step 1:Loading all data and concatenianting data for 12 data sets
 air_quality_data_set_file_paths = [
     r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Aotizhongxin_20130301-20170228.csv',
     r"C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Changping_20130301-20170228.csv",
@@ -15,20 +14,32 @@ air_quality_data_set_file_paths = [
     r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Huairou_20130301-20170228.csv',
     r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Nongzhanguan_20130301-20170228.csv',
     
-   r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Shunyi_20130301-20170228.csv',
-   r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Tiantan_20130301-20170228.csv',
-   r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Wanliu_20130301-20170228.csv',
-   r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Wanshouxigong_20130301-20170228.csv'
+    r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Shunyi_20130301-20170228.csv',
+    r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Tiantan_20130301-20170228.csv',
+    r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Wanliu_20130301-20170228.csv',
+    r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Wanshouxigong_20130301-20170228.csv'
 
   ]
 
 data_set_frames = [pd.read_csv(file) for file in air_quality_data_set_file_paths]
 df = pd.concat(data_set_frames)
 
-
 # Step 2: Get an overview of the data
 print("Find the number of row and column", df.shape)
 print("\nFind the number of data types and non-null values is count in data set")
+
 #step 3:Find the rows number of the dataset
 df.info()
 print("\nFirst Few Rows of the Dataset:")
+
+#step 4:Find the statistics value
+print(df.head())
+print("\nFind the number of statics value :")
+print(df.describe())
+
+
+# Step 5: Find the missing vlaue of the data set
+missing_values = df.isnull().sum()
+print("\nFinding missing vlaue of  air quality data  Values in Each Column Before Handling the data set:")
+print(missing_values[missing_values > 0])
+
