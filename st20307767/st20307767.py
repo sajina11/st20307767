8a8b7cea9b5097290fb8cf8deae540a68c6d3158
evadevkota <devkotaeva@gmail.com>
2024-12-16 17:20:21 +0000

    initial dataset

--- st20307767/st20307767.py
+++ st20307767/st20307767.py
@@ -1,10 +1,18 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+import streamlit as st
 import seaborn as sns
+import matplotlib.pyplot as plt
+import folium
+from folium.plugins import MarkerCluster
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
-#step 1:Loading all data and concatenianting data for 12 data sets
-air_quality_data_set_file_paths = [
-    r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Aotizhongxin_20130301-20170228.csv',
+# Define paths to all datasets
+data_paths = [
+       r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Aotizhongxin_20130301-20170228.csv',
     r"C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Changping_20130301-20170228.csv",
     r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Dingling_20130301-20170228.csv',
     r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Dongsi_20130301-20170228.csv',
@@ -19,28 +27,5 @@ air_quality_data_set_file_paths = [
     r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Wanliu_20130301-20170228.csv',
     r'C:\Users\Dell\OneDrive\Desktop\programmingdata\Dataset Folder for your Final Assessment-20241125\PRSA_Data_Wanshouxigong_20130301-20170228.csv'
 
-  ]
-
-data_set_frames = [pd.read_csv(file) for file in air_quality_data_set_file_paths]
-df = pd.concat(data_set_frames)
-
-# Step 2: Get an overview of the data
-print("Find the number of row and column", df.shape)
-print("\nFind the number of data types and non-null values is count in data set")
-
-#step 3:Find the rows number of the dataset
-df.info()
-print("\nFirst Few Rows of the Dataset:")
-
-#step 4:Find the statistics value
-print(df.head())
-print("\nFind the number of statics value :")
-print(df.describe())
-
-
-
-# Step 5: Find the missing vlaue of the data set
-missing_values = df.isnull().sum()
-print("\nFinding missing vlaue of  air quality data  Values in Each Column Before Handling the data set:")
-print(missing_values[missing_values > 0])
+ ]
 
